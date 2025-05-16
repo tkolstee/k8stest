363218d0b452e401663fb4abf24c6e800f2d4661
Tony Kolstee <tony@kolstee.net>
2025-05-16 00:48:48 -0400

    Add redis container, hit counter

--- flask-app/main.py
+++ flask-app/main.py
@@ -2,14 +2,26 @@
 
 import flask
 import socket
+import os
+import redis
 
 hostname = socket.gethostname()
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_port = os.getenv('REDIS_PORT', 6379)
+r = redis.Redis(host=redis_host, port=redis_port)
+# r.set("key", "value")
+# print(r.get("key"))
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"This is {hostname} running Flask!"
+    if not r.exists("hit_counter"):
+        r.set("hit_counter", 0)
+    hit_count = r.get("hit_counter")
+    hit_count += 1
+    r.set("hit_counter", hit_count)
+    return f"This is {hostname} running Flask. Hits: {hit_count}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
