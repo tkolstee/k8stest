5b54260c3ad8b64156dd217bab56a836406c3eb2
Tony Kolstee <tony@kolstee.net>
2025-05-16 01:30:01 -0400

    Make redis work. Hopefully.

--- flask-app/main.py
+++ flask-app/main.py
@@ -9,9 +9,6 @@ hostname = socket.gethostname()
 redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_port = os.getenv('REDIS_PORT', 6379)
 
-# r.set("key", "value")
-# print(r.get("key"))
-
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -19,10 +16,9 @@ def index():
     r = redis.Redis(host=redis_host, port=redis_port)
     if not r.exists("hit_counter"):
         r.set("hit_counter", 0)
-    hit_count = r.get("hit_counter")
-    hit_count += 1
+    hit_count = int(r.get("hit_counter")) + 1
     r.set("hit_counter", hit_count)
-    return f"This is {hostname} running Flask. Hits: {hit_count}!"
+    return f"This is {hostname} running Flask. Hits: {str(hit_count)}!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
